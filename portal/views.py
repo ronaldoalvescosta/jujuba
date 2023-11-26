bae926b8ff05b0858f1b544fcabff0642fe326dd
Ronaldo Alves da Costa <ronaldo.costa@ailos.coop.br>
2023-11-26 16:58:49 -0300

    wip: project config

--- portal/views.py
+++ portal/views.py
@@ -5,8 +5,11 @@ from .models import *
 ms_identity_web = settings.MS_IDENTITY_WEB
 
 def index(request):
-    return render(request, "portal/auth/status.html")
+    return render(request, "index.html")
+
+def status(request):
+    return render(request, "status.html")
 
 @ms_identity_web.login_required
 def token_details(request):
-    return render(request, "portal/auth/token.html")
+    return render(request, "token.html")
